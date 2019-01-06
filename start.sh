2d9bd78a47e9481a48c86700c76e4cd9a52276f9
Chris Holman <squid@sirsquidness.com>
2019-01-06 11:03:42 +1100

    Fix reference to incorrect image name

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-mkdir -p /data/data /data/logs
-docker rm -f cache
 
-docker run --name cache -d --restart=always -p 80:80 -v /data/data:/cache -v /data/logs:/var/log/nginx cache
+IMAGE=origin-docker
+NAME=cache
+
+mkdir -p /data/data /data/logs
+docker rm -f $NAME
+
+docker run --name $NAME -d --restart=always -p 80:80 -v /data/data:/cache -v /data/logs:/var/log/nginx $IMAGE
